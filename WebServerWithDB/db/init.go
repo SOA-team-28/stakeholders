f2e267c2671eeac3bdd02f0106ef5e4c9cf5bca7
Stefaan01 <116906194+Stefaan01@users.noreply.github.com>
2024-06-06 19:39:01 +0200

    fix: path

--- WebServerWithDB/db/init.go
+++ WebServerWithDB/db/init.go
@@ -12,7 +12,7 @@ var (
 )
 
 func InitDB() *gorm.DB {
-	dsn := "host=stakeholders_database user=postgres password=super dbname=stakeholders port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=super dbname=tours-microservice port=5432 sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		print(err)
